ffaeb491d8728e9455198f5b103e93ff2b1879c3
matikucharski <mati.kucharski@gmail.com>
2022-12-31 17:08:16 +0100

    update

---
+++ docs/global-imports.js
@@ -0,0 +1,26 @@
+/* Imports for global scope */
+
+Meteor = Package.meteor.Meteor;
+global = Package.meteor.global;
+meteorEnv = Package.meteor.meteorEnv;
+WebApp = Package.webapp.WebApp;
+$ = Package.jquery.$;
+jQuery = Package.jquery.jQuery;
+ReactiveVar = Package['reactive-var'].ReactiveVar;
+Tracker = Package.tracker.Tracker;
+Deps = Package.tracker.Deps;
+ReactiveDict = Package['reactive-dict'].ReactiveDict;
+LaunchScreen = Package['launch-screen'].LaunchScreen;
+Blaze = Package.ui.Blaze;
+UI = Package.ui.UI;
+Handlebars = Package.ui.Handlebars;
+Spacebars = Package.spacebars.Spacebars;
+Template = Package['templating-runtime'].Template;
+meteorInstall = Package.modules.meteorInstall;
+meteorBabelHelpers = Package.modules.meteorBabelHelpers;
+Promise = Package.promise.Promise;
+HTML = Package.htmljs.HTML;
+Symbol = Package['ecmascript-runtime-client'].Symbol;
+Map = Package['ecmascript-runtime-client'].Map;
+Set = Package['ecmascript-runtime-client'].Set;
+
